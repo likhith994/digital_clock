d55994862769f885e45fe2050c092b12bee3e354
likhith994 <ylikith72@gmail.com>
2026-06-19 11:12:34 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Clock from "./Clock"
+const App= () =>{
+    return <Clock />
+}
+export default App;
---
+++ src/components/Clock.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+let now=new Date();
+let hour=now.getHours();
+let minutes=now.getMinutes();
+let seconds=now.getSeconds();
+let period ="";
+if (hour === 0) {
+    hour = 12;
+    period = "AM";
+}else if (hour === 12) {
+    period = "PM";
+}else if (hour > 12) {
+    hour = hour - 12;
+    period = "PM";
+}else {
+    period = "AM";
+}
+const Clock= () =>{
+    return (<div className="container">
+        <h1>{hour}:{minutes}:{seconds} {period}</h1>
+    </div>);
+}
+export default Clock;
