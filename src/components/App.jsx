c0d4215d3323d4974f5ccfec05aa5d43b510b151
likhith994 <ylikith72@gmail.com>
2026-06-19 17:33:16 +0530

    fixed dark mode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Clock from "./Clock"
+const darkMode =true;
 const App= () =>{
-    return (<div><Clock /></div>
+    return (<div>
+        <button
+            className={`btn ${darkMode ? "" : "light-mode"}`}
+            onClick={() => {darkMode=false}}
+        >
+            light mode
+        </button>
+        <Clock />
+        </div>
     );
 }
+
+
 export default App;
