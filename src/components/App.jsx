bebd214b1a8f358b7751c5171cc9d6a0ec8eacee
likhith994 <ylikith72@gmail.com>
2026-06-19 18:29:27 +0530

    added two clocks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Clock from "./Clock"
-const darkMode =true;
-const App= () =>{
+const darkMode = true;
+const App = () => {
     return (<div>
         <button
             className={`btn ${darkMode ? "" : "light-mode"}`}
-            onClick={() => {darkMode=false}}
+            onClick={() => { darkMode = false }}
         >
             light mode
         </button>
         <Clock />
-        </div>
+        <Clock />
+    </div>
     );
 }
 
